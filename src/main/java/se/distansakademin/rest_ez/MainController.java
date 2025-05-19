45ce505bd93415025cf34c69910687c710532ed9
linus-rudbeck <linus.rudbeck@distansakademin.se>
2025-05-19 11:14:27 +0200

    Initial commit

---
+++ src/main/java/se/distansakademin/rest_ez/MainController.java
@@ -0,0 +1,15 @@
+package se.distansakademin.rest_ez;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    // http://localhost:8080/hello
+
+    @GetMapping("/hello")
+    public String getHello(){
+        return "Hello my friend";
+    }
+}
